353a076b4564f9f694fe9245fe64c969d6bb7132
Mateus Santos <65752974+M-de-Mateus@users.noreply.github.com>
2022-12-21 15:49:26 -0300

    Construindo os formulários

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import './home.css'
+
 export default function Home(){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
     return(
-      <div>
-        <h1>Página Home</h1>
+      <div className='home-container'>
+        <h1>Tarefas</h1>
+        <span>Anote todas suas tarefas de forma fácil!</span>
+
+        <form>
+          <input type='text' placeholder='Digite o seu email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input autoComplete={false} type='password' placeholder='******' value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+          <button type='submit'>Acessar</button>
+        </form>
       </div>
     )
   }
